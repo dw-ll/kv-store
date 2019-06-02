a2665b00c42b5139a2f55fad45d19345484cc6d6
Daniel Williams <danswilliams95@gmail.com>
2019-06-02 11:01:39 -0700

    adding shard.py

---
+++ shard.py
@@ -0,0 +1,24 @@
+import os
+import json
+import random
+
+
+class ReplicaGroup:
+    def __init__(self, node_id, count, members, keys, fingerTable):
+        self.shard_id = node_id
+        self.shard_count = count
+        self.shard_id_members = members
+        self.key_count = keys
+        self.finger_table = {}
+
+    def getReplicaGroupID(self):
+        return self.shard_id
+
+    def getReplicaGroupCount(self):
+        return self.shard_count
+
+    def getReplicas(self):
+        return self.shard_id_members
+
+    def getCountOfReplicas(self):
+        return self.keys
